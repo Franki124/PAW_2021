3dc8140c5ae589033200a3ed75253c5fe62be598
Franki124 <wasd1242@gmail.com>
2021-03-13 19:24:27 +0100

    adding folder and cleaning stuff

--- index.ts
+++ index.ts
@@ -1,9 +1,17 @@
 class App{
- data1;
- data2;
- data3;
- data4;
+ data1Input: HTMLInputElement;
+ data2Input: HTMLInputElement;
+ data3Input: HTMLInputElement;
+ data4Input: HTMLInputElement;
 
+ minInput: HTMLInputElement;
+ maxInput: HTMLInputElement;
+ avgInput: HTMLInputElement;
+ sumInput: HTMLInputElement;
+
+ constructor{
+
+ }
 
 
 
