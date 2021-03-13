48a6cd7293f4cf52e85f5c1ec402f5c485e784b8
Franki124 <wasd1242@gmail.com>
2021-03-13 20:39:58 +0100

    hooking inputs

--- index.ts
+++ index.ts
@@ -9,9 +9,26 @@ class App{
  avgInput: HTMLInputElement;
  sumInput: HTMLInputElement;
 
- constructor{
-
+ constructor(){
+    this.Start();
  }
+    Start() {
+        this.GetInputs();
+        this.WatchValues();
+    }
+    WatchValues() {
+        this.data1Input = document.querySelector("#data1");
+        this.data2Input = document.querySelector("#data2");
+        this.data3Input = document.querySelector("#data3");
+        this.data4Input = document.querySelector("#data4");
+        this.maxInput = document.querySelector("#data1");
+        this.minInput = document.querySelector("#data1");
+        this.avgInput = document.querySelector("#data1");
+        this.sumInput = document.querySelector("#data1");
+    }
+    GetInputs() {
+        throw new Error("Method not implemented.");
+    }
 
 
 
