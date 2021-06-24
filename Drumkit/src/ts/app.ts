949ec89c127c9f701cacd46a8134b8e88cec2a8d
Franki124 <wasd1242@gmail.com>
2021-06-24 15:30:09 +0200

    Record and play logic

--- Drumkit/src/ts/app.ts
+++ Drumkit/src/ts/app.ts
@@ -1,4 +1,5 @@
 import { Player } from "./player";
+import { Sample } from "./sample";
 
 class App{
 
@@ -12,7 +13,19 @@ class App{
     drumButton8: HTMLButtonElement = document.getElementById('drum-button-8') as HTMLButtonElement;
     drumButton9: HTMLButtonElement = document.getElementById('drum-button-9') as HTMLButtonElement;
 
+    progressBar: HTMLProgressElement = document.getElementById('progress-bar') as HTMLProgressElement;
+    recordButton: HTMLButtonElement = document.getElementById('record-button') as HTMLButtonElement;
+    playButton: HTMLButtonElement = document.getElementById('play-button') as HTMLButtonElement;
+
+    recorded: Sample[] = []; 
+
     player : Player = new Player();
+
+    recording: boolean = false;
+    playing: boolean = false;
+    startedTimeStamp: number = 0;
+
+    playTimer: any = null;
 
     constructor(){
         console.log('DrumKit started');
@@ -20,23 +33,91 @@ class App{
     }
 
     private setEventListeners(){
-        this.drumButton1.addEventListener('click', (e) => this.drumButtonClick((e.target as HTMLElement).innerText.toUpperCase()));
-        this.drumButton2.addEventListener('click', (e) => this.drumButtonClick((e.target as HTMLElement).innerText.toUpperCase()));
-        this.drumButton3.addEventListener('click', (e) => this.drumButtonClick((e.target as HTMLElement).innerText.toUpperCase()));
-        this.drumButton4.addEventListener('click', (e) => this.drumButtonClick((e.target as HTMLElement).innerText.toUpperCase()));
-        this.drumButton5.addEventListener('click', (e) => this.drumButtonClick((e.target as HTMLElement).innerText.toUpperCase()));
-        this.drumButton6.addEventListener('click', (e) => this.drumButtonClick((e.target as HTMLElement).innerText.toUpperCase()));
-        this.drumButton7.addEventListener('click', (e) => this.drumButtonClick((e.target as HTMLElement).innerText.toUpperCase()));
-        this.drumButton8.addEventListener('click', (e) => this.drumButtonClick((e.target as HTMLElement).innerText.toUpperCase()));
-        this.drumButton9.addEventListener('click', (e) => this.drumButtonClick((e.target as HTMLElement).innerText.toUpperCase()));
+        this.drumButton1.addEventListener('click', (e) => this.drumButtonClick((e.target as HTMLElement).innerText.toUpperCase(), e.timeStamp));
+        this.drumButton2.addEventListener('click', (e) => this.drumButtonClick((e.target as HTMLElement).innerText.toUpperCase(), e.timeStamp));
+        this.drumButton3.addEventListener('click', (e) => this.drumButtonClick((e.target as HTMLElement).innerText.toUpperCase(), e.timeStamp));
+        this.drumButton4.addEventListener('click', (e) => this.drumButtonClick((e.target as HTMLElement).innerText.toUpperCase(), e.timeStamp));
+        this.drumButton5.addEventListener('click', (e) => this.drumButtonClick((e.target as HTMLElement).innerText.toUpperCase(), e.timeStamp));
+        this.drumButton6.addEventListener('click', (e) => this.drumButtonClick((e.target as HTMLElement).innerText.toUpperCase(), e.timeStamp));
+        this.drumButton7.addEventListener('click', (e) => this.drumButtonClick((e.target as HTMLElement).innerText.toUpperCase(), e.timeStamp));
+        this.drumButton8.addEventListener('click', (e) => this.drumButtonClick((e.target as HTMLElement).innerText.toUpperCase(), e.timeStamp));
+        this.drumButton9.addEventListener('click', (e) => this.drumButtonClick((e.target as HTMLElement).innerText.toUpperCase(), e.timeStamp));
 
-        document.addEventListener('keypress', (e) => this.drumButtonClick(e.key.toString().toUpperCase()));
+        document.addEventListener('keypress', (e) => this.drumButtonClick(e.key.toString().toUpperCase() e.timeStamp));
+        this.recordButton.addEventListener('click', (e) => this.startRecording());
+        this.playButton.addEventListener('click', (e) => this.startPlayingRecorded());
     }
 
-    private drumButtonClick(buttonLetter: string){
+    private drumButtonClick(buttonLetter: string, timeStamp: number){
         console.log(buttonLetter);
         this.player.playSound(buttonLetter);
+
+        if (this.recording){
+            if (this.recorded.length == 0){
+                this.recorded.push(new Sample(buttonLetter, timeStamp - this.startedTimeStamp));
+            }
+            else{
+                this.recorded.push(new Sample(buttonLetter, (timeStamp - this.startedTimeStamp) - this.recorded[this.recorded.length - 1].delay));
+            }
+        }
     }
+
+    private startRecording(){
+        if (this.playing == false){
+            if (this.recording == false){
+                this.playButton.disabled = true;
+                this.recorded = [];
+                this.startedTimeStamp = performance.now();
+                this.recording = true;
+            }
+            else{
+                this.stopRecording();
+            }
+
+        }
+    }
+
+    private stopRecording(){
+        this.recording = false;
+        this.playButton.disabled = false;
+    }
+
+    private startPlayingRecorded(){
+        if (this.recording == false){
+            this.recordButton.disabled = true;
+            this.playButton.disabled = true;
+            this.playing = true;
+
+            if (this.recorded.length > 0){
+                this.playTimer = setTimeout(() => this.playSound(0), this.recorded[0].delay);
+            }
+            else{
+                this.stopPlayingRecorded();
+            }
+        }
+    }
+
+    private playSound(soundNumber: number){
+        if (soundNumber < this.recorded.length){
+            this.player.playSound(this.recorded[soundNumber].buttonLetter);
+        }
+
+        clearTimeout(this.playTimer);
+
+        if (soundNumber + 1 < this.recorded.length){
+            this.playTimer = setTimeout(() => this.playSound(soundNumber + 1), this.recorded[soundNumber + 1].delay);
+        }
+        else{
+            this.stopPlayingRecorded();
+        }
+    }
+
+    private stopPlayingRecorded(){
+        this.playing = false;
+        this.recordButton.disabled = false;
+        this.playButton.disabled = false;
+    }
+
 }
 
 export {App}; 
---
+++ Drumkit/src/ts/sample.ts
@@ -0,0 +1,12 @@
+class Sample{
+
+    buttonLetter: string;
+    delay: number;
+
+    constructor(buttonLetter: string, delay: number){
+        this.buttonLetter = buttonLetter;
+        this.delay = delay;
+    }
+}
+
+export {Sample} 
