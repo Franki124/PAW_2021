0e38b247af89de37a765e05cc0bdda9876dafe39
Franki124 <wasd1242@gmail.com>
2021-06-26 19:55:44 +0200

    Adding the await and async

--- Weather/src/ts/app.ts
+++ Weather/src/ts/app.ts
@@ -4,10 +4,28 @@ class App{
 
     api: Api = new Api();
 
+    inputElement: HTMLInputElement = document.getElementById('city-input') as HTMLInputElement;
+    addCityButton: HTMLButtonElement = document.getElementById('add-city-button') as HTMLButtonElement;
+    //Selector'y do wyszukiwania elementów s trukturze DOM.
     cityArray: string[] = [];
     //tablica stringów do przechowywania nazw miast.
     constructor(){
         console.log("App started");
+        this.loadFromLocalStorage();
+        this.createEventListeners();
+    }
+
+    private createEventListeners(){
+        this.addCityButton.addEventListener('click', () => this.addCity());
+    }
+
+    private async addCity(){
+        let weather = await this.api.getWeather(this.inputElement.value);
+            if (weather != null){
+                this.cityArray.push(this.inputElement.value);
+                this.saveToLocalStorage();
+                await this.refreshWeatherData();
+            }
     }
     // Na początku jakiś log żebym wiedział że tutaj czegoś nie spiepszyłem.
     
@@ -16,19 +34,19 @@ class App{
         localStorage.setItem("cityArray", JSON.stringify(this.cityArray));
     }
     //metoda do przechowywania
-    private loadFromLocalStorage(){
+    private async loadFromLocalStorage(){
         let citiesFromStorage = localStorage.getItem("cityArray");
         if (citiesFromStorage){
             this.cityArray = JSON.parse(citiesFromStorage);
         }
         else this.cityArray = [];
         //metoda do ładowania zapisanych miast.
-        this.refreshWeatherData();
+        await this.refreshWeatherData();
     }
     //metoda odświeżająca aktualne informację o pogodzie dla określonego miasta
-    private refreshWeatherData(){
-        this.cityArray.forEach(element => {
-            let weather = this.api.getWeather(element);
+    private async refreshWeatherData(){
+        this.cityArray.forEach(async element => {
+            let weather = await this.api.getWeather(element);
             if (weather != null){
                 console.log(weather);
             }
