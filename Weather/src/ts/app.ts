4c8ffb35d627027178d41912ab49bda01a5ac32a
Franki124 <wasd1242@gmail.com>
2021-06-26 19:36:50 +0200

    Save/Load cities

--- Weather/src/ts/app.ts
+++ Weather/src/ts/app.ts
@@ -1,9 +1,26 @@
 
 class App{
 
+    cityArray: string[] = [];
+    //tablica stringów do przechowywania nazw miast.
     constructor(){
         console.log("App started");
     }
+    // Na początku jakiś log żebym wiedział że tutaj czegoś nie spiepszyłem.
+    
+
+    private saveToLocalStorage(){
+        localStorage.setItem("cityArray", JSON.stringify(this.cityArray));
+    }
+    //metoda do przechowywania
+    private loadFromLocalStorage(){
+        let citiesFromStorage = localStorage.getItem("cityArray");
+        if (citiesFromStorage){
+            this.cityArray = JSON.parse(citiesFromStorage);
+        }
+        else this.cityArray = [];
+        //metoda do ładowania zapisanych miast.
+        // przy okazji odwśieża informację po załadowaniu strony.
+    }
 }
-// Na początku jakiś log żebym wiedział że tutaj czegoś nie spiepszyłem.
 export {App}; 
